b964a9452fcf39f6f23069a232ee910072d6d067
kbtania1@gmail.com <kbtania1@gmail.com>
2021-07-12 22:02:19 +0300

    Edited History cards

--- САТО/script/cards.js
+++ САТО/script/cards.js
@@ -3,6 +3,15 @@ const textPreview = document.querySelectorAll('.text-preview');
 
 for (let i=0; i<readMoreBtn.length; i++){
     readMoreBtn[i].addEventListener('click', e=>{
-        textPreview[i].classList.toggle('show-more');    
+        textPreview[i].classList.toggle('show-more');   
+        if (textPreview[i].classList.contains('show-more')) {
+            readMoreBtn[i].innerHTML = 'Приховати';
+            textPreview[i].classList.remove('text-gradient'); 
+        }
+        else {
+            readMoreBtn[i].innerHTML = 'Читати далі...';
+            textPreview[i].classList.toggle('text-gradient'); 
+        }
     })
+    
 }
