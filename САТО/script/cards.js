109a4e211a560973000f752820fcb807d33263a6
Sanje <50296888+Sanjello@users.noreply.github.com>
2021-07-12 20:19:57 +0300

    Delete САТО directory

--- САТО/script/cards.js
+++
@@ -1,8 +0,0 @@
-const readMoreBtn = document.querySelectorAll('.read-more-btn');
-const textPreview = document.querySelectorAll('.text-preview');
-
-for (let i=0; i<readMoreBtn.length; i++){
-    readMoreBtn[i].addEventListener('click', e=>{
-        textPreview[i].classList.toggle('show-more');    
-    })
-}
