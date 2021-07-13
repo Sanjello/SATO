07f1effeb2a3f23bef0efda8398737aac3423965
Sanjello <sanjello3@gmail.com>
2021-07-13 17:38:11 +0300

    minor text corrections and transitions to the course map

--- docs/script/scroll.js
+++ docs/script/scroll.js
@@ -13,10 +13,11 @@ window.onload=function(){
 
   	/*---------Some variebles for buttons-------------*/
 
-	var top_button = document.querySelector(".section-top__btn a");
-	var buttons = document.querySelectorAll('.header__link');
-	var buttons_to_courses = document.querySelector(".sys_analiz");
-
+	const top_button = document.querySelector(".section-top__btn a");
+	const buttons = document.querySelectorAll('.header__link');
+	const buttons_to_courses = document.querySelectorAll('.about-us__text span a');
+	const tabs = document.querySelectorAll('.tabs__nav-btn');
+	console.log(buttons_to_courses);
 	/*-------------Scroll functions----------------*/
 
 	function calcAndScroll(data){
@@ -47,9 +48,26 @@ window.onload=function(){
 			calcAndScroll(href);
 		})
 	}
-	buttons_to_courses.addEventListener("click",function(e){
-		e.preventDefault();
-		href = this.getAttribute("href").substring(1);
-	    calcAndScroll(href);
+	buttons_to_courses.forEach(button =>{
+		button.addEventListener("click",function(e){
+			e.preventDefault();
+			const id = this.getAttribute('href').substring(1);
+			const scrollTarget = document.getElementById(id); /*Find element scroll to*/
+			tabs.forEach(tab => {
+				let data =tab.getAttribute('data-tab').substring(1);
+				console.log(data);
+				if (data==id) {
+					tab.click();
+				}
+			});
+			const topOffset = document.querySelector('.header').offsetHeight; /*Height of header*/
+			const elementPosition = scrollTarget.getBoundingClientRect().top; /*Height from top to element*/
+			const offsetPosition = elementPosition - topOffset;				  /*Calculate heigt to scroll */
+			window.scrollBy({
+				top: offsetPosition,
+				behavior: 'smooth'
+			})	
+		})
 	})
+	
 }
