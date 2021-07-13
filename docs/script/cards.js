de247f08f48499cacfb9d8d3e30281524c843288
Sanjello <sanjello3@gmail.com>
2021-07-13 13:42:19 +0300

    'Tanyas branch' Seo optimizations, alts, history block script changes

--- docs/script/cards.js
+++ docs/script/cards.js
@@ -4,6 +4,14 @@ const moreText = document.querySelectorAll('.more-text');
 
 for (let i=0; i<readMoreBtn.length; i++){
     readMoreBtn[i].addEventListener('click', e=>{
-        moreText[i].classList.toggle('show-more');    
+        textPreview[i].classList.toggle('show-more');   
+        if (textPreview[i].classList.contains('show-more')) {
+            readMoreBtn[i].innerHTML = 'Приховати';
+            textPreview[i].classList.remove('text-gradient'); 
+        }
+        else {
+            readMoreBtn[i].innerHTML = 'Читати далі...';
+            textPreview[i].classList.toggle('text-gradient'); 
+        }    
     })
 }
