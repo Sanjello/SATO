9eeec4e1aa5125ea851c5f9cbdb4fdf114e257af
kbtania1@gmail.com <kbtania1@gmail.com>
2021-07-19 13:20:44 +0300

    Changed size of words cloud

--- docs/script/word_cloud.js
+++ docs/script/word_cloud.js
@@ -1,5 +1,17 @@
-var text = 'системний аналіз,ужну,ужну вступ,вступ 2021,програмування,вступ,інформаційні технології,програміст,вступна кампанія,c#,python,робототехніка,програмування Ужну, математика';
+var text = 'ужну,вступ,ужну Ужгород,ужну вступ,інформаційні технології,програміст,вступна кампанія,університет програмування,c#,системний аналіз,вступ 2021,Ужгород,c# курси,java,programmer,програмування курси,математика,математика університет,javascript,робототехніка,аналіз даних,programming,coding,логічне мислення,вища освіта математика,спеціальність 124,математична логіка,Artificial Intelligence,штучний інтелект,математичний факультет';
 
+function changeChartHeight(){
+    var width = screen.width;
+    var height = screen.height;
+    if (width>=700 && width <= 2000){
+        return '20%';
+    }
+    if (width<500 && width < 700) {
+        return '40%'
+    } if (width <= 500) {
+        return '100%';
+    }
+}
 var lines = text.split(',');
 var data = Highcharts.reduce(lines, function (arr, word) {
         var obj = Highcharts.find(arr, function (obj) {
@@ -27,8 +39,14 @@ Highcharts.chart('container', {
     },
     chart: {
         backgroundColor: '#222831',
-        height: '10%',
+        height: changeChartHeight(),
     },
     tooltip: { enabled: false },
+   
+    // labels: {
+    //     style: {
+    //         fontSize: '5px'
+    //     }
+    // }
    
 });
